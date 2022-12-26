57db1314ff309144cef57c4a2f231737a0c66953
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-12-26 21:25:50 +0530

    Added an appropriate scolding text to the system

--- frontend/src/reducers/itemList.js
+++ frontend/src/reducers/itemList.js
@@ -57,7 +57,7 @@ const reducer = (state = {}, action) => {
         tab: null,
         title: action.title,
         currentPage: 0,
-        // noResults: action.payload.items.length === 0,
+        noResults: action.payload.items.length === 0,
       };
     case HOME_PAGE_LOADED:
       return {
