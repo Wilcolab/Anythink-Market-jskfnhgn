4fff9148bde82e2fd67af17ea473af6f4eb33f3f
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-12-26 21:25:50 +0530

    Added an appropriate scolding text to the system

--- frontend/src/components/ItemList.js
+++ frontend/src/components/ItemList.js
@@ -6,7 +6,13 @@ const ItemList = (props) => {
   if (!props.items) {
     return <div className="py-4">Loading...</div>;
   }
-
+  if (props.noResults) {
+    return (
+      <div className="py-4 no-items" id="empty">
+        No items were found for your search :(
+      </div>
+    );
+  }
   if (props.items.length === 0) {
     return <div className="py-4 no-items">No items are here... yet.</div>;
   }
